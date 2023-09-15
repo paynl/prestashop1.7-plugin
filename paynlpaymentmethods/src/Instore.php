6725b7665ba91a0d94d3b604c6b2aa38eee02282
kevinverschoor <61683999+kevinverschoor@users.noreply.github.com>
2023-09-15 09:06:43 +0200

    PHP lint

--- paynlpaymentmethods/src/Instore.php
+++ paynlpaymentmethods/src/Instore.php
@@ -1,26 +1,25 @@
 <?php
 
-
 namespace PaynlPaymentMethods\PrestaShop;
 
 use PaynlPaymentMethods\PrestaShop\PaymentMethod;
 use db;
 
 class Instore extends PaymentMethod
-{   
+{
     /**
      * Check the status of the pin
      *
-     * @param $hash
-     * @param $transactionId
-     * @param $object
+     * @param string $hash
+     * @param string $transactionId
+     * @param string $object
      * @return \Paynl\Result\Instore\Status
      */
     public static function handlePin($hash, $transactionId, $object)
     {
         try {
             $status = \Paynl\Instore::status(['hash' => $hash]);
-            Db::getInstance()->execute("UPDATE `" . _DB_PREFIX_ . "pay_transactions` SET `status` = '" . $status->getTransactionState() . "', `updated_at` = now() WHERE `" . _DB_PREFIX_ . "pay_transactions`.`transaction_id` = '" . Db::getInstance()->escape($transactionId) . "';");
+            Db::getInstance()->execute("UPDATE `" . _DB_PREFIX_ . "pay_transactions` SET `status` = '" . $status->getTransactionState() . "', `updated_at` = now() WHERE `" . _DB_PREFIX_ . "pay_transactions`.`transaction_id` = '" . Db::getInstance()->escape($transactionId) . "';"); // phpcs:ignore
             if (in_array($status->getTransactionState(), ['cancelled', 'expired', 'error'])) {
                 $object->errors[] = $object->module->l('The payment could not be completed', 'finish');
                 $object->redirectWithNotifications('index.php?controller=order&step=1');
