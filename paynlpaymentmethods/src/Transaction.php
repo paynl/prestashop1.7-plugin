9634bcdce405d1796b65aa79875442e60c1c8899
Anne <anne@pay.nl>
2024-01-24 13:10:08 +0100

    Extra linter

--- paynlpaymentmethods/src/Transaction.php
+++ paynlpaymentmethods/src/Transaction.php
@@ -1,23 +1,23 @@
 <?php
 
-
 namespace PaynlPaymentMethods\PrestaShop;
 
 use Db;
 use DbQuery;
 
+/**
+ * @phpcs:disable Squiz.Commenting.FunctionComment.TypeHintMissing
+ */
 class Transaction
 {
-
     /**
      * Adds the transaction to the pay_transactions table
-     *
-     * @param integer $transaction_id
-     * @param integer $cart_id
-     * @param integer $customer_id
-     * @param integer $payment_option_id
-     * @param float $amount
-     *
+     * @param $transaction_id
+     * @param $cart_id
+     * @param $customer_id
+     * @param $payment_option_id
+     * @param $amount
+     * @return void
      */
     public static function addTransaction($transaction_id, $cart_id, $customer_id, $payment_option_id, $amount)
     {
@@ -35,17 +35,16 @@ class Transaction
     }
 
     /**
-     * Adds the pinnterminal hash to the transaction, this is required for the instore option
-     *
-     * @param int $transaction_id
-     * @param string $hash
-     *      
+     *  Adds the pinnterminal hash to the transaction, this is required for the instore option
+     * @param $transaction_id
+     * @param $hash
+     * @return void
      */
     public static function addTransactionHash($transaction_id, $hash)
     {
         $db = Db::getInstance();
 
-        $sql = "UPDATE `" . _DB_PREFIX_ . "pay_transactions` SET `hash` = '" . Db::getInstance()->escape($hash) . "', `updated_at` = now() WHERE `" . _DB_PREFIX_ . "pay_transactions`.`transaction_id` = '" . Db::getInstance()->escape($transaction_id) . "';";
+        $sql = "UPDATE `" . _DB_PREFIX_ . "pay_transactions` SET `hash` = '" . Db::getInstance()->escape($hash) . "', `updated_at` = now() WHERE `" . _DB_PREFIX_ . "pay_transactions`.`transaction_id` = '" . Db::getInstance()->escape($transaction_id) . "';"; // phpcs:ignore
         $db->execute($sql);
     }
 
