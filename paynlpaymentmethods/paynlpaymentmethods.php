75bf874b0b68fa5fff95a51201141d2912da64fc
woutse <wouter@pay.nl>
2025-03-13 15:34:42 +0100

    Bumped version

--- paynlpaymentmethods/paynlpaymentmethods.php
+++ paynlpaymentmethods/paynlpaymentmethods.php
@@ -67,7 +67,7 @@ class PaynlPaymentMethods extends PaymentModule
     {
         $this->name = 'paynlpaymentmethods';
         $this->tab = 'payments_gateways';
-        $this->version = '4.17.8';
+        $this->version = '4.17.9';
         $this->payLogEnabled = null;
         $this->ps_versions_compliancy = array('min' => '1.7', 'max' => _PS_VERSION_);
         $this->author = 'PAY.';
